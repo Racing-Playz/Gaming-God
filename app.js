65eac76747503f8ccac782e3298fe84e7a97ac34
RacingPlayz <72081031+Racing-Playz@users.noreply.github.com>
2020-09-30 05:19:45 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const config = require('./config.json');
 const Client = require('./src/Client.js');
 global.__basedir = __dirname;
 
 // setup
-const client = new Client(config);
+const Discord = require('discord.js');
+const bot = new Discord.Client();
 
 // initialize client
 function init() {
   client.loadEvents('./src/events');
   client.loadCommands('./src/commands');
-  client.login(client.token);
+  bot.login('NzYwNDY4MzkzNzQ4MzMyNTc2.X3MfYQ.Yt3wL2Nu46m4zIEm35P93Z52noI');
 }
 
 init();
